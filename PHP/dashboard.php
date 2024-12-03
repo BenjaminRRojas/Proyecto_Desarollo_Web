95ab2d7a4b80e992e95365e93bdf4a035d5a5b61
Benja <benjaminrojasnavea@gmail.com>
2024-12-03 00:03:01 -0300

    se hace un aside en el dashboard , se mejora el style_dash

--- PHP/dashboard.php
+++ PHP/dashboard.php
@@ -1,10 +1,7 @@
 <?php
-
 include("modulos CRUD/conexion.php");
-
 ?>
 
-?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -16,7 +13,22 @@ include("modulos CRUD/conexion.php");
 </head>
 <body>
 
-<video src="../imagenes/fondo.mp4" autoplay preload muted loop></video> 
+    <video src="../imagenes/fondo.mp4" autoplay preload muted loop></video>
+
+    <!-- Menú lateral -->
+    <div class="sidebar">
+        <h2>Menu</h2>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Cursos</a></li>
+            <li><a href="#">Estudiantes</a></li>
+            <li><a href="#">Inscripciones</a></li>
+            <li><a href="#">Reportes</a></li>
+            <li><a href="#">Configuraciones</a></li>
+        </ul>
+    </div>
+
+    <!-- Contenido principal -->
     <div class="dashboard-container">
         <h1>Dashboard de Cursos Online</h1>
 
@@ -36,7 +48,7 @@ include("modulos CRUD/conexion.php");
             </div>
         </div>
 
-        <!--Cursos mas populares ( con mas usuarios)-->
+        <!--Cursos más populares-->
         <h2>Cursos Más Populares</h2>
         <table>
             <thead>
