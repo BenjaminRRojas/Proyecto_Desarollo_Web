2147d687b68f18afbeefdb1459dcc61889742d7e
Martín Villagrán <martinvillagran340@gmail.com>
2024-12-08 13:15:15 -0300

    Se arreglan detalles respecto al breakpoint (920px)

--- PHP/Nosotros.php
+++ PHP/Nosotros.php
@@ -12,59 +12,60 @@
 <body>
     <video src="../imagenes/fondo.mp4" autoplay preload muted loop></video>
     <!--Navbar-->
-    <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
 
-                <a class="navbar-brand ms-3" href="index.html">
-                    <img src="../imagenes/logo.svg" alt="logo" height="125">
-                </a>
-
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="d-none d-lg-block text-center ms-5">
-                    <h1 class="navbar-title">Aprende a programar desde cero hasta el infinito</h1>
-                </div>
+                    <a class="navbar-brand ms-3" href="index.html">
+                        <img src="../imagenes/logo.svg" alt="logo" height="125">
+                    </a>
 
 
-                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
-                    aria-labelledby="offcanvasNavbarLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menú</h5>
-                        <button type="button" class="btn-close bg-light" data-bs-dismiss="offcanvas"
-                            aria-label="Close"></button>
-                        <hr>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="d-none d-lg-block text-center ms-5">
+                        <h1 class="navbar-title">Aprende a programar desde cero hasta el infinito</h1>
                     </div>
-                    <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-end">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Cursos</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle active" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">Perfil</a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Iniciar Sesión</a></li>
-                                    <li><a class="dropdown-item" href="#">Registrarse</a></li>
-                                </ul>
-                            </li>
-                        </ul>
+
+
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+                        aria-labelledby="offcanvasNavbarLabel">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menú</h5>
+                            <button type="button" class="btn-close bg-light" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
+                            <hr>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav justify-content-end">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="#">Cursos</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle active" href="#" role="button"
+                                        data-bs-toggle="dropdown" aria-expanded="false">Perfil</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Iniciar Sesión</a></li>
+                                        <li><a class="dropdown-item" href="#">Registrarse</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
 
-        <div class="container-fluid">
+        
             <!--Container Misión y Visión-->
-            <div class="flexMisionVision mt-5 justify-content-around">
-                <div class="text-left mision" id="id123">
-                    <div class="mt-4 ms-4 text-light">
+            <div class="flexMisionVision justify-content-around">
+                <div class="mision">
+                    <div class="mt-4 ms-4 text-light titulo">
                         <h1 class="titulo">Misión y Visión</h1>
                     </div>
                     <div class="mt-4 mb-5 ms-5 me-5 fs-5 lh-lg text-light justificar">
@@ -72,7 +73,7 @@
                         <li class="mt-3">Visualizamos un mundo donde el aprendizaje sea accesible, personalizado y continuo, promoviendo el crecimiento individual y contribuyendo al progreso global a través de la formación de comunidades de aprendizaje inclusivas y colaborativas.</li>
                     </div>
                 </div>
-                <img class="mb-2 img-fluid misionImg" src="../imagenes/Estudiante.webp" alt="Estudiante">
+                <img class="img-fluid misionImg" src="../imagenes/Estudiante.webp" alt="Estudiante">
             </div>
             <!--Container Equipo-->
             <div class="flexEquipo mt-5 mb-5 justify-content-around">
@@ -147,7 +148,7 @@
 
 
     </div>
-    </div>
+</div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     
