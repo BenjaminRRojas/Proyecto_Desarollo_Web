e0ba4cdcd7eef453f7951a169f50e1d3feca10d9
Benjamin Acevedo <benjacevedo1@gmail.com>
2024-12-24 02:04:46 -0300

    arreglar pequeño error cone

--- PHP/Modulos/DOCENTES/VISTAS/formulario.php
+++ PHP/Modulos/DOCENTES/VISTAS/formulario.php
@@ -10,9 +10,9 @@ $usuarios = $controlador->listarUsuarios();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Usuarios</title>
+    <title>Lista de Docentes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../../../../CSS/style_estudiantes.css">
+    <link rel="stylesheet" href="../../../../CSS/style-dashdocentes.css">
 </head>
 
 <body>
@@ -22,7 +22,7 @@ $usuarios = $controlador->listarUsuarios();
 
     <!-- Contenedor principal -->
     <div class="container mt-5">
-        <h1 class="text-center">Lista de Estudiantes</h1>
+        <h1 class="text-center">Lista de Docentes</h1>
         
         <!-- Buscador -->
         <div class="row my-4">
