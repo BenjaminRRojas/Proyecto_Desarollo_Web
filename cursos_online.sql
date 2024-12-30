11abb0af7f61c0e63cd768b3a3bf6817c96bd2f0
Benjamin Acevedo <benjacevedo1@gmail.com>
2024-12-29 21:20:42 -0300

    se cambio DB

--- cursos_online.sql
+++ cursos_online.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3307
--- Tiempo de generación: 30-12-2024 a las 01:14:35
+-- Tiempo de generación: 30-12-2024 a las 01:19:20
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
